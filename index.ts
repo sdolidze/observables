df87618089694fa5dc755a41d58b2a2bbf2c22a8
Sandro Dolidze <me@sdolidze.ge>
2018-08-20 13:15:25 +0400

    implement flatMap

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import {
+  getLoanByCustomerId,
+  getLoanAndCustomerByCustomerId,
+} from './app/api/loan.api';
+
+getLoanByCustomerId(1).subscribe(loan => {
+  console.log(loan);
+});
+
+getLoanAndCustomerByCustomerId(2).subscribe(([customer, loan]) => {
+  console.log(customer, loan);
+});
+
+getLoanByCustomerId(20).subscribe(loan => {
+  console.log(loan);
+});
